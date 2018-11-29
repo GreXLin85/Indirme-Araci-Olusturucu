ae0c51f22f9fd61b1727e3f3ccc4bc9ba00f20e3
GreXLin85 <38141908+GreXLin85@users.noreply.github.com>
2018-11-29 13:49:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import urllib.request
 import os
-print("=İndirme aracı oluşturucu=")
+print("==İndirme aracı oluşturucu==")
 print("Not: İndirilecek dosyaları sıkıştırılmış yani zip, rar gibi formatlarda yükleyiniz.")
 baslik = input("Başlık: ")
 link = input("İndirilecek dosya (örn. ornek.com/ornek.zip): ")
@@ -22,5 +22,5 @@ print("Kurulum tamamlandi")''')
 ia.close()
 urllib.request.urlretrieve("https://www.dropbox.com/s/1g1q5ufru08oy78/7z.exe?dl=1","7z.exe")
 urllib.request.urlretrieve("https://www.dropbox.com/s/9x694bj1bl97sgc/7z.dll?dl=1","7z.dll")
-print("İşlemler tamamlandı dosya ismi: ia.py")
+print("İşlemler tamamlandı dosya ismi: "+indirmedosyaisim+".py\nLütfen "+indirmedosyaisim+".py ve 7z.exe 7z.dll isimli dosyaları yanyana tutunuz aksi taktirde hata alabilirsiniz!")
 input("Çıkmak için enter basın")
